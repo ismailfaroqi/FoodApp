264c3de9d580d4efb8dd728c34c5a337cbd52a70
Ismail Faroqi <Ismail.Faroqi@atlantica.dev>
2022-05-21 09:55:11 +0700

    Menambahkan FoodService nya

---
+++ FoodService/GraphQL/Mutation.cs
@@ -0,0 +1,63 @@
+﻿using HotChocolate.AspNetCore.Authorization;
+using FoodService.Models;
+
+namespace FoodService.GraphQL
+{
+    public class Mutation
+    {
+        //ADD
+        [Authorize(Roles = new[] { "MANAGER" })]
+        public async Task<Food> AddFoodAsync(
+           FoodInput input,
+            [Service] Latihan5Context context)
+        {
+
+            // EF
+            var food = new Food
+            {
+                Name = input.Name,
+                Stock = input.Stock,
+                Price = input.Price,
+                Created = DateTime.Now
+            };
+
+            var ret = context.Foods.Add(food);
+            await context.SaveChangesAsync();
+
+            return ret.Entity;
+        }
+        //UPDATE 
+        [Authorize(Roles = new[] { "MANAGER" })]
+        public async Task<Food> UpdateFoodAsync(
+            FoodInput input,
+            [Service] Latihan5Context context)
+        {
+            var food = context.Foods.Where(o => o.Id == input.Id).FirstOrDefault();
+            if (food != null)
+            {
+                food.Name = input.Name;
+                food.Stock = input.Stock;
+                food.Price = input.Price;
+
+                context.Foods.Update(food);
+                await context.SaveChangesAsync();
+            }
+            return await Task.FromResult(food);
+        }
+
+        //DELETE
+        [Authorize(Roles = new[] { "MANAGER" })]
+        public async Task<Food> DeleteFoodByIdAsync(
+            int id,
+            [Service] Latihan5Context context)
+        {
+            var food = context.Foods.Where(o => o.Id == id).FirstOrDefault();
+            if (food != null)
+            {
+                context.Foods.Remove(food);
+                await context.SaveChangesAsync();
+            }
+            return await Task.FromResult(food);
+        }
+    }
+}
---
+++ FoodService/GraphQL/Query.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Claims;
+using HotChocolate.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using FoodService.Models;
+
+namespace FoodService.GraphQL
+{
+    public class Query
+    {
+        [Authorize]
+        public IQueryable<Food> GetFoods([Service] Latihan5Context context, ClaimsPrincipal claimsPrincipal)
+        {
+            var userName = claimsPrincipal.Identity.Name;
+
+            // check buyer role ?
+            var buyerRole = claimsPrincipal.Claims.Where(o => o.Type == ClaimTypes.Role && o.Value == "BUYER").FirstOrDefault();
+            var food = context.Users.Where(o => o.Username == userName).FirstOrDefault();
+            if (food != null)
+            {
+                if (buyerRole != null)
+                    return context.Foods;
+
+                var foods = context.Foods.Where(o => o.Id == food.Id);
+                return foods.AsQueryable();
+            }
+            return new List<Food>().AsQueryable();
+        }
+    }
+}
